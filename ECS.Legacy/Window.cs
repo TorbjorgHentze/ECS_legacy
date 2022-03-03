afbfd2e2d812cf0dbfe20ba19ad36e94fb9000a8
Cecilie Lynggaard Jeppesen <au661804@uni.au.dk>
2022-03-03 13:14:24 +0100

    Pakker installeret

--- ECS.Legacy/Window.cs
+++ ECS.Legacy/Window.cs
@@ -1,7 +1,17 @@
-﻿namespace ECS_v2
+﻿using System;
+
+namespace ECS_v2
 {
-    public class Window
+    public class Window:IRegulate
     {
-        
+        public void TurnOn()
+        {
+            Console.WriteLine("Window is open");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Window is closed");
+        }
     }
 }
